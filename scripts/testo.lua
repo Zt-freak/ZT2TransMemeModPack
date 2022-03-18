10c28c499e96167c40d601cf91199189ee729ff4
Apodemus Erectus <bnkho98@hotmail.com>
2022-03-18 09:53:26 +0100

    added testosterone

---
+++ scripts/testo.lua
@@ -0,0 +1,14 @@
+include "scenario/scripts/misc.lua"
+include "modules/ZtModArchive/ArluqTools/scripts/arluq.lua"
+
+function transition (args)
+    aq.try(
+        function ()
+            local subjectAnimal = resolveTable(args[1].value)
+            if !aq.animal.getGender(subjectAnimal) then
+                aq.animal.setGender(subjectAnimal, "_M")
+                displayZooMessageTextWithZoom("The testosterone is taking effect on " .. aq.animal.getName(subjectAnimal) .. ".", 1, 30, subjectAnimal)
+            end
+        end
+    )
+end
